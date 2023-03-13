8e158c95509ebd23734c598538f4ba79a1f2c533
claireYng <claraclaritayng@gmail.com>
2023-03-13 16:30:35 +0700

    Create FormPeminjaman.java

---
+++ src/perpustakaan/FormPeminjaman.java
@@ -0,0 +1,20 @@
+package perpustakaan;
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.table.DefaultTableModel;
+
+public class FormPeminjaman {
+    public void show(){
+
+    }
+    public void tambahBuku(Buku, int lama){
+
+    }
+    public void tampilPinjaman(ArrayList<Buku){
+
+    }
+    public void hapusBuku(BukuDipinjam) {
+        
+    }
+}
