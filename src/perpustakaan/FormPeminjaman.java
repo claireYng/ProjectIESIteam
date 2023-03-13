c119c82c34f5d7f2ea65c743283bd803691ab787
claireYng <claraclaritayng@gmail.com>
2023-03-13 16:53:33 +0700

    Update FormPeminjaman.java

--- src/perpustakaan/FormPeminjaman.java
+++ src/perpustakaan/FormPeminjaman.java
@@ -17,4 +17,22 @@ public class FormPeminjaman {
     public void hapusBuku(BukuDipinjam) {
         
     }
+    private void initComponents() {
+
+        jJudulBuku = new javax.swing.JTextField();
+        jButtonCari = new javax.swing.JButton();
+        jButtonKonfirmasi = new javax.swing.JButton();
+        jLama = new javax.swing.JTextField();
+        jdaftarPinjaman = new javax.swing.JTable();
+        jDaftarBuku = new javax.swing.JTable();
+
+       // setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+       /*  jButtonCari.setText("Cari");
+        jButtonCari.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonCariMouseClicked(evt);
+            }*/
+        });
+
 }
