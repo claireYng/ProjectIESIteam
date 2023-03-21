2764ea1d6a5c403e0859e67eef752899fbd8602e
Eci Emriyeni <eciemriyeni@student.ub.ac.id>
2023-03-21 14:31:36 +0700

    Update PeminjamanController.java

--- src/perpustakaan/PeminjamanController.java
+++ src/perpustakaan/PeminjamanController.java
@@ -5,10 +5,27 @@ import java.util.ArrayList;
 
 public class PeminjamanController {
     public void showFormPeminjaman(){
-
+        Perpustakaan.formPeminjaman = new FormPeminjaman();
+        Perpustakaan.formPeminjaman.show();
     }
     public void cariBuku(String judul){
-
+        BukuProvider bukuProvider = new BukuProvider();
+        try{
+            ArrayList<Buku> listBuku = bukuProvider.selectBuku(judul);
+            if(listBuku.isEmpty())
+            {
+                DialogUI dialogUI = new DialogUI("Buku tidak terdaftar");
+                dialogUI.pack();;
+                dialogUI.setLocationRelativeTo(null);
+                dialogUI.setVisible(true);
+            }
+            else Perpustakaan.formPeminjaman.tampilPinjaman(listBuku);
+        } catch(Exception ex) {
+            DialogUI dialogUI = new DialogUI("Connection Error");
+            dialogUI.pack();
+            dialogUI.setLocationRelativeTo(null);
+            dialogUI.setVisible(true);
+        }
     }
     public void pinjam (BukuDipinjam[]){
 
