62f0ac424bf3f910283413efa7709c53a7fbe2f1
claireYng <claraclaritayng@gmail.com>
2023-04-06 20:29:53 +0700

    baru
    baru

--- src/perpustakaan/PeminjamanController.java
+++ src/perpustakaan/PeminjamanController.java
@@ -1,25 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package perpustakaan;
 
 import java.awt.Dialog;
 import java.util.ArrayList;
 
-public class PeminjamanController {
-    public void showFormPeminjaman(){
+public class peminjamanController {
+    
+    public void showFormPeminjaman() {
         Perpustakaan.formPeminjaman = new FormPeminjaman();
-        Perpustakaan.formPeminjaman.show();
+        Perpustakaan.formPeminjaman.tampilkan(); 
     }
-    public void cariBuku(String judul){
+    
+    public void cariBuku(String judul) {
         BukuProvider bukuProvider = new BukuProvider();
-        try{
+        try {
             ArrayList<Buku> listBuku = bukuProvider.selectBuku(judul);
             if(listBuku.isEmpty())
             {
                 DialogUI dialogUI = new DialogUI("Buku tidak terdaftar");
-                dialogUI.pack();;
+                dialogUI.pack();
                 dialogUI.setLocationRelativeTo(null);
                 dialogUI.setVisible(true);
-            }
-            else Perpustakaan.formPeminjaman.tampilPinjaman(listBuku);
+            } 
+            else Perpustakaan.formPeminjaman.display(listBuku);
         } catch(Exception ex) {
             DialogUI dialogUI = new DialogUI("Connection Error");
             dialogUI.pack();
@@ -27,7 +34,5 @@ public class PeminjamanController {
             dialogUI.setVisible(true);
         }
     }
-    public void pinjam (BukuDipinjam[]){
-
-    }
+    
 }
